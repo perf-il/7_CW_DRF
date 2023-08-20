acbd827ce7e7a730ea065bdc49199e7bd22e7bfb
Ilia Perfilov <ind.climb@gmail.com>
2023-08-20 19:03:49 +0300

    urls for habit was created

---
+++ habits/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+
+from habits.apps import HabitsConfig
+from habits.views import HabitViewSet
+
+app_name = HabitsConfig.name
+
+router = DefaultRouter()
+router.register(r'habits', HabitViewSet, basename='habits')
+
+urlpatterns = router.urls
