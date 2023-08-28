9c20cf71b05e3187525ce21cbd2d8dae38b9f7fd
Ilia Perfilov <ind.climb@gmail.com>
2023-08-28 18:13:55 +0300

    admin.py was updated

--- habits/admin.py
+++ habits/admin.py
@@ -5,5 +5,5 @@ from habits.models import Habit
 
 @admin.register(Habit)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('user', 'location', 'time', 'action', 'is_enjoyable', 'related_habit','periodicity',
+    list_display = ('user', 'location', 'time', 'action', 'is_enjoyable', 'related_habit', 'periodicity',
                     'reward', 'execution_time', 'is_public', )
